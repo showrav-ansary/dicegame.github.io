3c72a353156a2ae379ca769e31c1883f4f2271ae
showrav-ansary <showrav.ansary.bd@gmail.com>
2022-06-29 10:27:49 +0600

    random progress saving

--- script.js
+++ script.js
@@ -7,17 +7,33 @@ const buttonNewGame = document.querySelector('.btn--new');
 const buttonRollDice = document.querySelector('.btn--roll');
 const buttonHold = document.querySelector('.btn--hold');
 
+const scores = [0,0];
+
 let currentScore = 0;
 
 let player = 0;
 
+
+const highLightPlayer = function(){
+    const playerPlaying = document.querySelector(`player--${player}`);
+    playerPlaying.classList.add('player--active');
+    const deactivatedPlayer = document.querySelector(`player--${player === 0 ? 1 :  0}`);
+    deactivatedPlayer.classList.remove('player--active');
+
+}
+
+const changePlayer = ()=> player === 0 ? player = 1 : player = 0;
+
+
 const getScore = function (player) {
-    return Number(document.getElementById(`score--${player}`).textContent);
+    return Number(document.getElementById(`current--${player}`).textContent);
 }
 
 const changeScore = (player, score) => {
     document.getElementById(`score--${player}`).textContent = score;
 }
+
+const changeTotalScore = (score)=>  document.getElementById(`current--${player}`).textContent = score;
 
 const resetGame = () => {
     changeScore(0, 0);
@@ -31,6 +47,16 @@ const rollDice = () => {
     const diceNumber = Math.trunc(Math.random() * 6) + 1;
     dice.src = `dice-${diceNumber}.png`;
     dice.classList.remove('hidden');
+    if (diceNumber === 1) {
+        currentScore = 0;
+        changePlayer();
+        highLightPlayer();
+    } else {
+        currentScore = getScore(player);
+        currentScore += diceNumber;
+        if (player ===0) playerOneScore.textContent = currentScore;
+        else playerTwoScore.textContent = currentScore;
+    }
 }
 
 buttonRollDice.addEventListener('click', rollDice);
